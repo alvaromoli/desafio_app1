69154a4d21228288770c11e68866f6b9ea2d1023
Álvaro Molina <abusmolina@gmail.com>
2019-06-03 20:31:18 -0400

    ADD files to config/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,22 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'code')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '20x20')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '40x40')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-bg-images', '40x40')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-css')
+Rails.application.config.assets.precompile += %w( *.jpg  *.gif *.png *.eot *.svg *.ttf *.woff *.otf)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
